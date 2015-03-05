8f4a60a84dfe20ec057f78aede77f4aa0e334d62
William Bernal <willtf.wb@gmail.com>
2015-03-04 19:41:11 -0500

    Correccion scroll

--- js/jsScroll.js
+++ js/jsScroll.js
@@ -1,31 +1,23 @@
-var iteracion=0;
-$(function(){
-		$(".contenido").mousewheel(function(event, delta) {
-			//alert(this.scrollLeft -= (delta * 1));
+$(function()
+{
+	$('.Home').click(function(){
+	  			
+	  	$('body,html').animate({scrollTop: $(".contenedor").offset().top},"slow");
+  				
+	 });
+		$(".contenido").mousewheel(function(event, delta) {	
 			this.scrollLeft -= (delta * 100);
 			event.preventDefault();
+			if(delta > 0 && this.scrollLeft < 1 )
+			{
+				$('body,html').animate({scrollTop: $("body").offset().top},"slow");
+			}
 		});   
-
-		$(window).scroll(function() {
-	  		var scrolltop =$(this).scrollTop();
-	  		//var scrollL=$('.contenido').scrollLeft();
-	  		
-	  		if(scrolltop > 10 && iteracion==0){
-	  			$('body,html').animate({scrollTop: $(".contenido").offset().top},"slow");
-	  			iteracion=1;
-	  			$('.volverArriba').css("display","block");
+		
+		$(window).mousewheel(function(event, delta) {
+	  		if(delta < 0){
+	  			$('body,html').animate({scrollTop: $(".contenido").offset().top},"fast");
   			}
-	  		$('.volverArriba').click(function(){
-	  			if(iteracion==1){$('body,html').animate({scrollTop: $("body").offset().top},"slow");
-  				}
-	  		});
-	  		if(scrolltop < 100 && iteracion==1){
-		  		iteracion=0;
-		  		$('.volverArriba').css("display","none");
-  			}
-  			/*if(scrollL < 4 && iteracion==1){
-	  			$('body,html').animate({scrollTop: $("body").offset().top},"slow");
-  			}*/
-  			
   		})
-	});
+
+});
